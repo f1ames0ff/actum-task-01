7b4566f67b4951c544f3b7762aa1a5283807493f
flamesoff <flamesoff@gmail.com>
2021-04-28 22:12:30 +0200

    fixed github token problem;

--- src/api/rest.api.ts
+++ src/api/rest.api.ts
@@ -4,14 +4,24 @@ import { UserSearchResult } from "../types/user-search-result.interface";
 
 export abstract class AppApi {
   private static readonly BASE_URL = 'https://api.github.com';
+  private static USER_TOKEN = 'Z2hwX2t0NTFEcnN5SE1LSHN5UnZQMk1XVGNVSW5HeU9ZRzNBS0lUSA==';
   private static readonly HEADERS = {
-    Accept: 'application/vnd.github.v3+json',
-    Authorization: 'token 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'
+    Accept: 'application/vnd.github.v3+json'
   };
+
+  static get token() {
+    return atob(this.USER_TOKEN);
+  }
 
   static setDefaults() {
     axios.interceptors.request.use((config) => {
-      config.headers = { ...config.headers, ...this.HEADERS };
+      config.headers = {
+        ...config.headers,
+        ...this.HEADERS,
+        ...{
+          Authorization: `token ${this.token}`
+        }
+      };
 
       return config;
     });
